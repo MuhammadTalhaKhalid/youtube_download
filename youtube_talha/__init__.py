388fc6b35b771af36f4f68d2404832b1a11f8355
MuhammadTalhaKhalid <80006163+MuhammadTalhaKhalid@users.noreply.github.com>
2025-05-15 13:19:35 +0000

    Update __init__.py

--- youtube_talha/__init__.py
+++ youtube_talha/__init__.py
@@ -1,6 +1,1 @@
-"""YouTube video downloader package by Talha"""
-from .downloader import download_highest_quality
-from .cli import main
 
-__version__ = "1.0.0"
-__all__ = ['download_highest_quality', 'main']
